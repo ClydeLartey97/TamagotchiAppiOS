8a5db81e08884156bc0f1d3790e34476a532e231
Clyde Lartey <lartey.d@etoncollege.org.uk>
2022-01-20 12:17:23 +0000

    Initial Commit

---
+++ TamagotchiAppiOS/TamagotchiAppiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiAppiOSApp.swift
+//  TamagotchiAppiOS
+//
+//  Created by Lartey, David (HWTA) on 20/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiAppiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
